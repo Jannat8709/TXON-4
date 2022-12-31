24ad251893276fd651023f2a62a7597023922a7c
Jannat8709 <119395182+Jannat8709@users.noreply.github.com>
2022-12-31 17:00:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("script");
+
+document.querySelector('.cross').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click",()=>{
+   document.querySelector('.sidebar').classList.toggle('sidebar-go')
+    if(document.querySelector('.sidebar').classList.contains('sidebar-go')){
+
+        document.querySelector('.Line').style.display = 'inline'
+        document.querySelector('.cross').style.display = 'none'
+        
+    }
+    else{
+        document.querySelector('.Line').style.display = 'none'
+        setTimeout(()=>{
+
+            document.querySelector('.cross').style.display = 'inline'
+        },300)
+        
+    }
+})
+
